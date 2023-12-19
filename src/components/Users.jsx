45d75e9fa50ebdf8e0749d06aa7de77248d87c73
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-19 14:48:11 +0200

    list users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,37 @@
-function Users() {
-  // parsisiusti usersius ir iskonsolinti
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
+const baseUrl = 'http://localhost:3000/api';
+
+function Users() {
+  const [usersArr, setUsersArr] = useState([]);
+  // parsisiusti usersius ir iskonsolinti
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/users`)
+      .then((ats) => {
+        console.log('ats ===', ats);
+        setUsersArr(ats.data);
+      })
+      .catch((error) => {
+        console.warn('ivyko klaida:', error);
+      });
+  }, []);
   // sugeneruoti html
+
+  console.log('usersArr ===', usersArr);
 
   return (
     <div>
       <h2>Users</h2>
+      <ul className='unlisted'>
+        {usersArr.map((uObj) => (
+          <li key={uObj.id}>
+            (id: {uObj.id}) {uObj.name} yra is {uObj.town}. vairuoja:{' '}
+            {uObj.isDriver ? 'Taip' : 'Ne'}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
