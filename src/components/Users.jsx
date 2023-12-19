cf6759f95f305ece0890e512d3c02f0e0ac641f8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-19 15:15:10 +0200

    users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -5,6 +5,9 @@ const baseUrl = 'http://localhost:3000/api';
 
 function Users() {
   const [usersArr, setUsersArr] = useState([]);
+  const [nameVal, setNameVal] = useState('');
+  const [townVal, setTownVal] = useState('');
+  const [isDriver, setIsDriver] = useState(false);
   // parsisiusti usersius ir iskonsolinti
   useEffect(() => {
     axios
@@ -21,9 +24,64 @@ function Users() {
 
   console.log('usersArr ===', usersArr);
 
+  function handleNewUserSubmit(event) {
+    event.preventDefault();
+    console.log('js is in control');
+    // sudeti viska i viena obj
+    // siusiuti ta ob i back
+    // pavyko ar ne
+  }
+
+  console.log('nameVal ===', nameVal);
+
   return (
     <div>
       <h2>Users</h2>
+
+      <h3>Add new user</h3>
+      <form onSubmit={handleNewUserSubmit} className='border p-4 '>
+        <div className='mb-3'>
+          <label htmlFor='name' className='form-label'>
+            Name
+          </label>
+          <input
+            value={nameVal}
+            onChange={(e) => setNameVal(e.target.value)}
+            type='text'
+            className='form-control'
+            id='name'
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='town' className='form-label'>
+            Town
+          </label>
+          <input
+            value={townVal}
+            onChange={(e) => setTownVal(e.target.value)}
+            type='text'
+            className='form-control'
+            id='town'
+          />
+        </div>
+        <div className='mb-3 form-check'>
+          <input
+            value={isDriver}
+            onChange={(e) => setIsDriver(e.target.checked)}
+            type='checkbox'
+            className='form-check-input'
+            id='exampleCheck1'
+          />
+          <label className='form-check-label' htmlFor='exampleCheck1'>
+            Driver
+          </label>
+        </div>
+
+        <button type='submit' className='btn btn-outline-info'>
+          Create
+        </button>
+      </form>
+
       <ul className='list-group'>
         {usersArr.map((uObj) => (
           <li className='list-group-item' key={uObj.id}>
