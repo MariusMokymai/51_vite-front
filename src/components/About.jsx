f055a809842ba5161a15b5d066ca9b391926c162
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-09 15:00:25 +0200

    about

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2 className='display-3'>About</h2>
+      <p className='lead'>Lorem, ipsum dolor.</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+        exercitationem sapiente illum iusto consectetur facere excepturi velit,
+        ullam saepe veritatis sit nulla eum repellat est natus ab fuga quod
+        temporibus.
+      </p>
+    </div>
+  );
+}
+
+export default About;
